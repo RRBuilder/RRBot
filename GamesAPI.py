7defc37b94ed638b8e2175abec820d3f93dae6f9
RRBuilder <73187048+RRBuilder@users.noreply.github.com>
2021-12-10 18:59:00 +0000

    Removed useless count

--- GamesAPI.py
+++ GamesAPI.py
@@ -9,40 +9,62 @@ def GameRequest(UUID):
     Map = []
     TimeEnded = []
     TimeStarted = []
+    game = []
     API_Status = True
     try:
         game = requests.get("https://api.hypixel.net/recentgames?key="+key+"&uuid="+UUID).json()
     except:
+        print("Something went wrong fetching the recent games!")
         API_Status = False
-    if len(game["games"]) == 0:
-        pass
+        return GameType, Map, TimeEnded, TimeStarted, API_Status
+
+    r = requests.head("https://api.hypixel.net/recentgames?key="+key+"&uuid="+UUID)
+    if r.status_code != 200:
+        print("The recent games API did not return a status code of 200, it returned "+str(r.status_code))
+        try:
+            GameType, Map, TimeEnded, TimeStarted = GameProcess(game)
+        except:
+            API_Status = False
+        return GameType, Map, TimeEnded, TimeStarted, API_Status
     else:
-        for x in range(len(game["games"])):
-            count = x+1
-            if count > 5:
-                break
-            else:
-                try:
-                    GameType.append(str(game["games"][x]["gameType"]))
-                except:
-                    GameType.append(str("No data"))
+        if len(game["games"]) == 0:
+            pass
+        else:
+            GameType, Map, TimeEnded, TimeStarted = GameProcess(game)
 
-                try:
-                    Map.append(str(game["games"][x]["map"]))
-                except:
-                    Map.append(str("No data"))
+        return GameType, Map, TimeEnded, TimeStarted, API_Status
 
-                try:
-                    TimeEnded.append(int(game["games"][x]["ended"]))
-                except:
-                    TimeEnded.append(int(0))
+def GameProcess(game):
+    GameType = []
+    Map = []
+    TimeEnded = []
+    TimeStarted = []
+    for x in range(len(game["games"])):
+        count = x+1
+        if count > 5:
+            break
+        else:
+            try:
+                GameType.append(str(game["games"][x]["gameType"]))
+            except:
+                GameType.append(str("No data"))
 
-                try:
-                    TimeStarted.append(int(game["games"][x]["date"]))
-                except:
-                    TimeStarted.append(int(0))
+            try:
+                Map.append(str(game["games"][x]["map"]))
+            except:
+                Map.append(str("No data"))
 
-    return GameType, Map, TimeEnded, TimeStarted, API_Status
+            try:
+                TimeEnded.append(int(game["games"][x]["ended"]))
+            except:
+                TimeEnded.append(int(0))
+
+            try:
+                TimeStarted.append(int(game["games"][x]["date"]))
+            except:
+                TimeStarted.append(int(0))
+
+    return GameType, Map, TimeEnded, TimeStarted
 
 @timed_lru_cache(600)
 def GamesList(uuid):
